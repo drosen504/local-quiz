d523573d9c60709039a1a5730cbf8c5bc91cba0c
Dan Rosen <drosen84@gmail.com>
2017-12-15 21:34:04 -0600

    started new project. put in questions

---
+++ app.js
@@ -0,0 +1,35 @@
+'use strict';
+/*global*/
+
+const QUESTIONS = [
+    {
+        qNum: 'Question 1 of 5',
+        text: 'What do you get when you mix salt and water?',
+        options: ['Lemonade', 'Taco surprise', 'Salt water', 'Glue'],
+        correctAnswerIndex: 'Salt water'
+    },
+    {
+        qNum: 'Question 2 of 5',
+        text: 'How are spiders made?',
+        options: ['Spider eggs', 'Mixing ants and caterpillars', 'Stepping on M&Ms', 'Asking nicely'],
+        correctAnswerIndex: 'Spider eggs'
+    },
+    {
+        qNum: 'Question 3 of 5',      
+        text: 'What is my middle name?',
+        options: ['Alexander', 'Lewis', 'No clue', 'Richard'],
+        correctAnswerIndex: 'No clue'
+    },
+    {
+        qNum: 'Question 4 of 5',
+        text: 'What happens during a solar eclipse?',
+        options: ['Water vapor from the ocean extinguishes the sun', 'The moon blocks our view of the sun', 'There is no such thing as a solar eclipse', 'The moon gets jealous and does something about it'],
+        correctAnswerIndex: 'The moon blocks our view of the sun'
+    },
+    {
+        qNum: 'Question 5 of 5',
+        text: 'Where do fish live?',
+        options: ['The sky', 'In water', 'In volcanoes', 'In a tree',],
+        correctAnswerIndex: 'In water'
+    }
+];
