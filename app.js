55a6722d8a5f0e2bae2b7e379752a115d23f1033
Dan Rosen <drosen84@gmail.com>
2017-12-15 22:20:47 -0600

    added some html. added quiz start event listener/handler

--- app.js
+++ app.js
@@ -6,31 +6,31 @@ const QUESTIONS = [
         qNum: 'Question 1 of 5',
         text: 'What do you get when you mix salt and water?',
         options: ['Lemonade', 'Taco surprise', 'Salt water', 'Glue'],
-        correctAnswerIndex: 'Salt water'
+        correctAns: 'Salt water'
     },
     {
         qNum: 'Question 2 of 5',
         text: 'How are spiders made?',
         options: ['Spider eggs', 'Mixing ants and caterpillars', 'Stepping on M&Ms', 'Asking nicely'],
-        correctAnswerIndex: 'Spider eggs'
+        correctAns: 'Spider eggs'
     },
     {
         qNum: 'Question 3 of 5',      
         text: 'What is my middle name?',
         options: ['Alexander', 'Lewis', 'No clue', 'Richard'],
-        correctAnswerIndex: 'No clue'
+        correctAns: 'No clue'
     },
     {
         qNum: 'Question 4 of 5',
         text: 'What happens during a solar eclipse?',
         options: ['Water vapor from the ocean extinguishes the sun', 'The moon blocks our view of the sun', 'There is no such thing as a solar eclipse', 'The moon gets jealous and does something about it'],
-        correctAnswerIndex: 'The moon blocks our view of the sun'
+        correctAns: 'The moon blocks our view of the sun'
     },
     {
         qNum: 'Question 5 of 5',
         text: 'Where do fish live?',
         options: ['The sky', 'In water', 'In volcanoes', 'In a tree',],
-        correctAnswerIndex: 'In water'
+        correctAns: 'In water'
     }
 ];
 
@@ -43,11 +43,28 @@ const defaultStore = function() {
     };
 };
 
+//Event Listeners
 
+//Event Listeners/Handlers
+const handleStart = function() {
+    $('.intro').on('click', '.js-click-start', event => {
+        event.preventDefault();
+        store = defaultStore;
+        store.currentQuestion = 0;
+        render();
+}
+
+//Renderer
+
+//HTML Templates
+
+//Etc
+
+//after DOM loads
 $(function() {
-    renderIntro();
-    handleClickStart();
-    handleQuestionSubmit();
-    handleNextQuestion();
+    render();
+
+    defaultStore();
+
 
 });
