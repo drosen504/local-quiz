b04cba9ce50b81186596b22b7e3993ecbe98887a
Dan Rosen <drosen84@gmail.com>
2018-01-09 15:30:15 -0600

    added handler.js file. linked script via html

---
+++ handler.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const handleStart = function() {
+  console.log('handleStart ran');     
+  STORE.currentQuestion = 0;
+  STORE.currentView = 'question';
+  STORE.userScore = 0;
+  console.log(`after handleStart, we're on question ${STORE.currentQuestion}`);
+  renderQuizHtml();
+  render();
+};
+  
+const handleSubmission = function() {
+  event.preventDefault();
+  const selection = $('input:checked').val();
+  console.log(`user chose ${selection}`);
+  STORE.userAnswer = selection;
+  
+  if (STORE.userAnswer === QUESTIONS[STORE.currentQuestion].correctAnswer) {
+    STORE.feedback = 'That is correct!';
+    STORE.userScore++;
+  }
+  else {
+    STORE.feedback = `Sorry. The correct answer was: ${QUESTIONS[STORE.currentQuestion].correctAnswer}`;
+  }
+  STORE.currentView = 'feedback';
+  generator.generateFeedback();
+  generator.generateScore();
+  render();
+};
+  
+const updateQuestion = function() {
+  $('.js-feedback').on('click', '.next-question', function() {
+    console.log('Next Question Clicked');
+    if (STORE.currentQuestion < QUESTIONS.length - 1) {
+      STORE.currentQuestion++;
+      STORE.currentView = 'question';
+      render();
+      generator.generateQuestionNumber();
+      generator.generateQuestion();
+      generator.generateAnswers();
+      console.log(`after updateQuestion, we're on question ${STORE.currentQuestion}`);
+    } else {
+      STORE.currentView = 'results';
+      render();
+      generator.generateResults();
+    }
+  });
+};
